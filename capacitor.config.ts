1def0c8d4bf4f48080ea1b4e879fe9b6a48e803e
Tanatep Pichetvasin <tep3@hotmail.com>
2026-02-25 11:37:51 +0700

    add lockfile, capacitor config, bundled scanner lib

---
+++ capacitor.config.ts
@@ -0,0 +1,13 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.thaiapp.qrforge',
+  appName: 'QR Forge',
+  webDir: 'public',
+  server: {
+    url: 'https://qr-code.thaiapp.com',
+    cleartext: true
+  }
+};
+
+export default config;
